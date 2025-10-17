b93008837db0147b47a85702954c17ead2800839
Nahia Beraza <nahia.beraza06@somo.eus>
2025-10-17 10:37:59 +0200

    Eliminada la carpeta de PracticaAlumnado

---
+++ Ejercicio Formula/monoplaza.php
@@ -0,0 +1,11 @@
+<?php
+    class Monoplaza{
+        protected $nombre_piloto;
+        protected $nacionalidad;
+        protected $numero_monoplaza;
+        protected $escuderia;
+        protected $puntos;
+
+        
+    }
+?>
